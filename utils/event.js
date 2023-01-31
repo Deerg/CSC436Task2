16d15d3bf310a6ca6d63fc053c40de5b9da37114
Deerg <deerg666@gmail.com>
2023-01-31 12:44:55 -0500

    Multiplication implemented

--- utils/event.js
+++ utils/event.js
@@ -3,6 +3,7 @@ import { add } from "./math";
 const eventHandler = () => {
 
     const addButton = document.getElementById('add');
+    const multButton = document.getElementById('mult');
     const number1 = document.getElementById('number-1');
     const number2 = document.getElementById('number-2');
     const result = document.getElementById('result');
@@ -11,9 +12,13 @@ const eventHandler = () => {
             // parseInt(number1.value, 10)+parseInt(number2.value,10);
             add(number1.value,number2.value);
     }
-
+    const updateDomWithMultNumbers = () => {
+        result.innerHTML = 
+            // parseInt(number1.value, 10)+parseInt(number2.value,10);
+            number1.value * number2.value;
+    }
     addButton.addEventListener('click', updateDomWithAddedNumbers);
-
+    multButton.addEventListener('click', updateDomWithMultNumbers);
 }
 
 export  {eventHandler};
